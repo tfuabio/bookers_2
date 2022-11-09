63a123cecd7c8d5e83916dc790b4d4baa526f73a
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-11-09 04:59:28 +0000

    [Update] 詳細ビュー, 編集ビュー、update機能、destroy機能の作成, フラッシュメッセージcreate時

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,6 +15,20 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
